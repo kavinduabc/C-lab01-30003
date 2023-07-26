632813c488474b52b4051b7c0adbbb2175ebc730
kavinduabc <132426449+kavinduabc@users.noreply.github.com>
2023-07-26 20:50:24 +0530

    Add files via upload

---
+++ c lab 1.1.c
@@ -0,0 +1,13 @@
+//c lab 1
+#include<stdio.h>
+int main()
+{
+    printf("*\n");
+    printf("**\n");
+    printf("***\n");
+    printf("****\n");
+    printf("*****\n");
+    printf("\n\n\n");
+    printf("*\n**\n***\n****\n*****");
+}
+
